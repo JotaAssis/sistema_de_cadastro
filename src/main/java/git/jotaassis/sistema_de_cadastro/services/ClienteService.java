be4687c6725e1b1c9bee56e7f34385059f480c6f
João Paulo <assislearn@gmail.com>
2024-12-22 19:20:26 -0300

    Entidade EnderecoApiViaCep criada para integração da API do ViaCEP

--- src/main/java/git/jotaassis/sistema_de_cadastro/services/ClienteService.java
+++ src/main/java/git/jotaassis/sistema_de_cadastro/services/ClienteService.java
@@ -1,8 +1,10 @@
 package git.jotaassis.sistema_de_cadastro.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import git.jotaassis.sistema_de_cadastro.dtos.ClienteDTO;
 import git.jotaassis.sistema_de_cadastro.repository.ClienteRepository;
 import git.jotaassis.sistema_de_cadastro.util.Response;
 
@@ -14,4 +16,8 @@ public class ClienteService {
 
     @Autowired
     private Response rm;
+
+    public ResponseEntity<?> cadastrar(ClienteDTO clienteDTO){
+        return null;
+    }
 }
---
+++ src/main/java/git/jotaassis/sistema_de_cadastro/services/EnderecoApiViaCep.java
@@ -0,0 +1,27 @@
+package git.jotaassis.sistema_de_cadastro.services;
+
+import java.util.Map;
+
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class EnderecoApiViaCep {
+    
+    private final String ViaCep_Url = "https://viacep.com.br/ws/";
+
+    public Map<String, String> buscarCep(String cep) {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = ViaCep_Url + cep + "/json/";
+        ResponseEntity<Map<String, String>> response = restTemplate.exchange(
+                url,
+                org.springframework.http.HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<Map<String, String>>() {
+                });
+        return response.getBody();
+    }
+
+}
