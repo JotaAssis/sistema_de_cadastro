5aa8da806ff87255dcb4c708baedae1129834c33
João Paulo <assislearn@gmail.com>
2024-12-22 19:29:02 -0300

    Metodo para cadastrar criado na ClienteService, e criada a classe ClienteController

---
+++ src/main/java/git/jotaassis/sistema_de_cadastro/controllers/ClienteController.java
@@ -0,0 +1,25 @@
+package git.jotaassis.sistema_de_cadastro.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import git.jotaassis.sistema_de_cadastro.dtos.ClienteDTO;
+import git.jotaassis.sistema_de_cadastro.models.Cliente;
+import git.jotaassis.sistema_de_cadastro.services.ClienteService;
+
+@RestController
+@RequestMapping("/cliente")
+public class ClienteController {
+    
+    @Autowired
+    private ClienteService clienteService;
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<Cliente> cadastrar(@RequestBody ClienteDTO clienteDTO){
+        return clienteService.cadastrar(clienteDTO);
+    }
+}
--- src/main/java/git/jotaassis/sistema_de_cadastro/services/ClienteService.java
+++ src/main/java/git/jotaassis/sistema_de_cadastro/services/ClienteService.java
@@ -1,10 +1,12 @@
 package git.jotaassis.sistema_de_cadastro.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import git.jotaassis.sistema_de_cadastro.dtos.ClienteDTO;
+import git.jotaassis.sistema_de_cadastro.models.Cliente;
 import git.jotaassis.sistema_de_cadastro.repository.ClienteRepository;
 import git.jotaassis.sistema_de_cadastro.util.Response;
 
@@ -17,7 +19,24 @@ public class ClienteService {
     @Autowired
     private Response rm;
 
-    public ResponseEntity<?> cadastrar(ClienteDTO clienteDTO){
-        return null;
+    @Autowired
+    private EnderecoApiViaCep enderecoViaCep;
+
+    public ResponseEntity<Cliente> cadastrar(ClienteDTO clienteDTO){
+        var endereco = enderecoViaCep.buscarCep(clienteDTO.getCep());
+
+        Cliente cliente = new Cliente();
+
+        cliente.setNome(clienteDTO.getNome());
+        cliente.setEmail(clienteDTO.getEmail());
+        cliente.setTelefone(clienteDTO.getTelefone());
+        cliente.setCep(clienteDTO.getCep());
+        cliente.setEndereco(endereco.get("logradouro"));
+        cliente.setBairro(endereco.get("bairro"));
+        cliente.setCidade(endereco.get("localidade"));
+        cliente.setEstado(endereco.get("uf"));
+
+        clienteRepository.save(cliente);
+        return new ResponseEntity<Cliente>(cliente, HttpStatus.CREATED);
     }
 }
