5a59b6146bec4f22739a3acc94355109dd8ebc1a
João Paulo <assislearn@gmail.com>
2024-12-22 18:46:37 -0300

    Projeto criado

---
+++ src/test/java/git/jotaassis/sistema_de_cadastro/SistemaDeCadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package git.jotaassis.sistema_de_cadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeCadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
